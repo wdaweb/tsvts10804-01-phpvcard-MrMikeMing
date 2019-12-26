231328455f63e79cd95351756ba4ab97da2481ae
MrMike <xuan0833695466@gmail.com>
2019-12-26 21:43:13 +0800

    作業上傳
    第一次

--- index.php
+++ index.php
@@ -1,13 +1,70 @@
-<?php
-/**
- * 設計`index.php`畫面為表單網頁，表單欄位可輸入
- *	- 姓名
- *	- 職稱
- *	- 聯絡電話
- *	- 電子信箱
- *	- 技能簡介
- *	- 人像照片
- *	- 下拉選單選擇版型
- *	- 按鈕有[預覽]，能提交form資料到api.php
- */
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>vCard產生器</title>
+  <?php
+  session_start();
+  //if
+  //if(!empty($_SESSION['log']))header('location:preview.php');
+  ?>
+  <style>
+  body{
+    background-color: #fff1eb;
+  }
+  .main {
+    background: radial-gradient(ellipse,#fff1eb, #ace0f9);
+    border-radius: 6rem;
+    margin:10% auto;
+    text-align: center;
+    width: 600px;
+  }
+  .data {
+    
+    text-align: left;
+    padding: 0 3rem 3rem 3rem;
+    border: none;
+    margin-bottom: 20px;
+  }
+</style>
+</head>
+<body>
+  
+
+
+
+<div class="main">
+  <h1>vCard產生器</h1>
+  <form action="api.php" method="post" enctype="multipart/form-data">
+    <div class="data">
+      <p>人像照 | <input type="file" name="png" size="50"></p>
+      <p>你的名字 | <input type="text" name="user" value="泰山小飛俠05號"size="60"></p>
+      <p>職位名稱 | <input type="text" name="title" value="前端小飛象/無敵解碼王/衣服神奈川"size="60"></p>
+      <p>手機號碼 | <input type="text" name="phone" value="0988-883-393"size="60"></p>
+      <p>E-Mail  | <input type="mail" name="mail" value="xuan0833695466@gmail.com" size="60"></p>
+      <p>
+        <span style="vertical-align:top">自我介紹 | </span>
+        <textarea name="skill" cols=65 rows=10>Lorem ipsum dolor sit amet consectetur adipisi
+          cing elit. Ipsa eveniet molestias odit, unde quasi officiis cupiditate beatae a sequi cumque maiores commodi i
+          taque explicabo optio sint saepe voluptas magnam quod!Quo id accusamus atque obcaecati ipsum, perspiciatis fugit nisi ex pe
+          rferendis doloribus animi, ...資料夾照片有一張JPG檔供使用著參考喔!!!!!!!!!!!</textarea>
+      </p>
+      
+      <hr>
+      <p style="text-align:center;">版型選擇 |
+        <select name="layout">
+          <option value="FlipCard" selected>FlipCard</option>
+          <option value="Hover1">Hover1</option>
+          <option value="Animated">Animated</option>
+        </select>
+        <input type="submit" value="送出預覽">
+      </p>
+    </div>
+    
+  </form>
+</div>
+</body>
+</html>
--- preview.php
+++ preview.php
@@ -1,6 +1,38 @@
 <?php
-/**
- * 設計`preview.php`，主要是能自動依版型(link到layout.css)跟session資訊(或cookie) 整合到HTML，成為完整的vCard畫面。
- * 另外提供返回按鈕，設計為`<button onclick="window.history.go(-1)">回頁面</button>`
- */
+session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" type="text/css" href="css/<?=$_SESSION['info']['layout']?>.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Lilita+One|Noto+Sans+TC|Pacifico&display=swap" rel="stylesheet">
+	<title>vCard產生器-<?=$_SESSION['info']['layout']?></title>
+</head>
+<body>
+	
+	<div class="card-container">
+	<div class="filp-card">
+	<div class="card-front">
+		<img src="img/<?=$_SESSION['info']['png']?>">
+	</div>
+	<div class="card-back details">
+		<h2><?=$_SESSION['info']['user']?></h2>
+		<h4>職位名稱: 
+		<?=$_SESSION['info']['title']?></h4>
+		<div>E-mail: 
+		<?=$_SESSION['info']['mail']?></div>
+		<div>手機號碼: 
+		<?=$_SESSION['info']['phone']?></div><br>
+		<div><h3>自我介紹<br></h3>
+		<?=$_SESSION['info']['skill']?></div><br>
+		<button class="btn" onclick="window.history.go(-1)">回頁面</button>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
+
+
